5edb68ec54c6a404fc20cf28e0d67381485de275
hilal aissani <hilalaissani@gmail.com>
2018-09-19 20:14:59 -0400

    nn

--- server.js
+++ server.js
@@ -34,6 +34,68 @@ server.post("/zoos", (req, res) => {
     });
 });
 
+// server.put("/zoos/:id", (req, res) => {
+//   const changes = req.body;
+//   const id = req.params.id;
+//   console.log("changes ===>", changes);
+//   console.log("id ===>", id);
+//   db("zoos")
+//     .where({ id: id })
+//     // .select("name")
+//     .update(changes)
+//     .then(count => {
+//       console.log("count ===>", count); // number of records
+
+//       if (count) {
+//         db("zoos")
+//           .select("name")
+//           .where({ id: id })
+//           .then(zoo => {
+//             res.status(201).json({ msg: zoo });
+//           })
+//           .catch(err => {
+//             res.status(500).json(err);
+//           });
+//       } else {
+//         res.status(404).json({ msg: "the zoo not found" });
+//       }
+//     })
+//     .catch(err => {
+//       res.status(500).json({ msg: err });
+//     });
+// });
+
+server.put("/zoos/:id", (req, res) => {
+  const changes = req.body;
+  const { id } = req.params;
+
+  db("zoos")
+    .where("id", "=", id) // or .where({ id: id })
+    .update(changes)
+    .then(count => {
+      // count === number of records updated
+      res.status(200).json(count);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.delete("/zoos/:id", (req, res) => {
+  const { id } = req.params;
+
+  db("zoos")
+    .where({ id }) // or .where(id, '=', id)
+    .del()
+    .then(count => {
+      // count === number of records deleted
+      res.status(200).json(count);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
 server.listen(port, () => {
   console.log(`=== server is running on port ${port} ===`);
 });
